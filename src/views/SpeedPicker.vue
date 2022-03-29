f05c1a6fb41263dbd79d36e7bd1999870f455602
JUKOOK <wnrnrdl@gmail.com>
2022-03-29 21:58:58 +0900

    chore: naver-map start

--- src/views/SpeedPicker.vue
+++ src/views/SpeedPicker.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="speed-picker"></div>
+  <div class="speed-picker">
+      <div id="naver-map"></div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+</script>
 
 <style scoped>
-    .speed-picker {
-        background-color: #fff;
-    }
-    
+.speed-picker {
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+}
+#naver-map {
+    width: 50%;
+    height: 50%;
+}
 </style>
