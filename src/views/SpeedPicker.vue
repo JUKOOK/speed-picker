a081d6fe6850b1e6d63c431054cc4c2156bcee92
JUKOOK <wnrnrdl@gmail.com>
2022-03-29 21:23:44 +0900

    feat: App.vue and base.css

---
+++ src/views/SpeedPicker.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="speed-picker"></div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+    .speed-picker {
+        background-color: #fff;
+    }
+    
+</style>
